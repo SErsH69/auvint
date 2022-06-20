6d9a676cc42711acbc80716132bb19aac118ab02
Sergey Goncharovр <doimpos@gmail.com>
2022-06-20 07:09:00 +0500

    blocks in main page done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
-$(function() {
+$(document).ready(function(){
     $('.main_map__hide').click(function() {
         $(this).remove();
     });
-})
+    $('.js_sl_news').slick({
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: false
+    });
+    $('.js_sl_certificates').slick({
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: false
+    });
+    $('.js_sl_vendors').slick({
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: false
+    });
+    $('.js_sl_slider').slick({
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true
+    });
+    $('.js_sl_project').slick({
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: false
+    });
+    $('.js_sl_first').slick({
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true
+    });
+});
